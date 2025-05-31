db24bce4fcbbe8748fad45e328a7023f413a7605
YuryLandau <yurylandau@gmail.com>
2025-05-31 16:52:36 -0300

    feat: Atualizar Readme.md e setup.py para build

--- README.md
+++ README.md
@@ -1,27 +1,34 @@
-# package_name
+# landau-image-processing
 
 Description. 
-The package package_name is used to:
-	- 
-	-
+The package image-processing is used to:
+	Processing
+		- Histogram matching
+		- Structural similarity
+	Utils
+		- Read image
+		- Save image
+		- Plot image
+		- Plot result
+		- Plot histogram.
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install landau-image-processing
 
 ```bash
-pip install package_name
+pip install landau-image-processing
 ```
 
 ## Usage
 
 ```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
+from landau-image-processing.processing import combination
+combination.combine_images()
 ```
 
 ## Author
-My_name
+Yury Landau
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,15 +7,27 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="landau-image-processing",
     version="0.0.1",
     author="Yury Landau",
     author_email="yurylandau@gmail.com",
     description="My short description",
+    
+    # Adiciona uma descrição longa ao pacote
+    # Isso é útil para fornecer mais informações sobre o pacote
+    # e pode ser exibido em repositórios como o PyPI.
+    # O conteúdo da descrição longa pode ser escrito em Markdown
+    # ou reStructuredText, dependendo do que você preferir.
+    # Aqui, estamos usando o conteúdo do arquivo README.md como descrição longa.
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="git@github.com:YuryLandau/image-processing-package.git",
+    url="https://github.com/YuryLandau/image-processing-package",
+    
+    # Especifica os pacotes a serem incluídos
+    # Se você não quiser incluir todos os pacotes, pode usar find_packages(exclude=["tests*"])
     packages=find_packages(),
+
+    # Instala dependências, caso o pacote tenha dependências de outros pacotes.
     install_requires=requirements,
-    python_requires='>=3.8',
+    python_requires='>=3.10',
 )
